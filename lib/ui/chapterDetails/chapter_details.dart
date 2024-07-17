01cb95eccb52909b4ec8fa69911c394e49b9603e
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2024-07-17 21:09:51 +0300

    feat:add chapter details and show it

---
+++ lib/ui/chapterDetails/chapter_details.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+import '../DefaultScreen.dart';
+import '../theme/MyTheme.dart';
+import 'VerseContent.dart';
+
+
+class ChapterDetails extends StatefulWidget {
+  const ChapterDetails({super.key});
+
+  static const String routeName = 'ChapterDetails';
+
+  @override
+  State<ChapterDetails> createState() => _ChapterDetailsState();
+}
+
+class _ChapterDetailsState extends State<ChapterDetails> {
+
+  List<String> verses=[];
+
+  @override
+  Widget build(BuildContext context) {
+
+    var arg = ModalRoute.of(context)?.settings.arguments as ChapterDetailsArg;
+
+    // list still loading
+    if (verses.isEmpty) readFileData(arg.chapterIndex);
+
+
+    return DefaultScreen(body:  Scaffold(
+      appBar: AppBar(
+        title:Text(arg.chapterName),
+      ),
+      body: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+          elevation: 24,
+          margin: const EdgeInsets.symmetric(vertical: 64,horizontal: 24),
+
+
+          child: verses.isEmpty? const Center(child: CircularProgressIndicator())
+              :ListView.separated(itemBuilder:(context,index){
+            return VerseContent(index, verses[index]);
+          }, itemCount: verses.length,
+            separatorBuilder: (BuildContext context, int index)=>
+                Container(
+                  height: 1,
+                  color: MyThemeData.lightPrimary,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 64),
+                ),
+
+          )
+      ),
+    ));
+
+
+  }
+
+  void readFileData(int fileIndex) async{
+    String fileContent =await rootBundle.loadString('assets/files/${fileIndex+1}.txt');
+
+    List<String> lines=fileContent.trim().split('\n');
+
+    //show data
+    setState(() {
+      verses =lines;
+    });
+
+  }
+}
+class ChapterDetailsArg {
+  int chapterIndex;
+  String chapterName;
+
+  ChapterDetailsArg(this.chapterIndex, this.chapterName);
+
+}
